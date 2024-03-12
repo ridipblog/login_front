cc2fd4f8d8eff715dd9fc67e6bc40da4b4bf1009
unknown <memorytemp5@gmail.com>
2024-03-12 21:29:10 +0530

    changed server address

--- src/components/base/baseServer.js
+++ src/components/base/baseServer.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://remove-rounds-cash-peter.trycloudflare.com",
     headers: {
         'Content-Type': 'application/json'
     }
