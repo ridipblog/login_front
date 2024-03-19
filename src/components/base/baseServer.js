9d89328cf7f117af99f363b6e98ea13a2d14443b
unknown <memorytemp5@gmail.com>
2024-03-19 21:34:09 +0530

    frontend chaneg 2

--- src/components/base/baseServer.js
+++ src/components/base/baseServer.js
@@ -2,6 +2,8 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://login-backend-dph4.onrender.com/",
     headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
         'Content-Type': 'application/json'
     }
 });
